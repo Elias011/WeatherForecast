c106ba8fd3163736cc32daad9ed384aedae0b65e
Elias011 <elias.shkshb1991@gmail.com>
2018-03-23 00:34:31 +0100

    awsome photos

--- js/app.js
+++ js/app.js
@@ -1,45 +1,3 @@
-// (function($, document, window){
-	
-// 	$(document).ready(function(){
-
-// 		// Cloning main navigation for mobile menu
-// 		$(".mobile-navigation").append($(".main-navigation .menu").clone());
-
-// 		// Mobile menu toggle 
-// 		$(".menu-toggle").click(function(){
-// 			$(".mobile-navigation").slideToggle();
-// 		});
-
-// 		var map = $(".map");
-// 		var latitude = map.data("latitude");
-// 		var longitude = map.data("longitude");
-// 		if( map.length ){
-			
-// 			map.gmap3({
-// 				map:{
-// 					options:{
-// 						center: [latitude,longitude],
-// 						zoom: 15,
-// 						scrollwheel: false
-// 					}
-// 				},
-// 				marker:{
-// 					latLng: [latitude,longitude],
-// 				}
-// 			});
-			
-// 		}
-// 	});
-
-// 	$(window).load(function(){
-
-// 	});
-
-// })(jQuery, document, window);
-
-
-//let x = findLocation.value;
-// let find = document.getElementById("find-button");
  function handelCityName(){
 	let findLocation = document.getElementById("find-location-co").value;
     document.getElementById("location-name").innerHTML = findLocation;
@@ -47,7 +5,15 @@
     let xReq = new XMLHttpRequest();
     xReq.open('GET',nameCity);
       xReq.onload =function result(){
-        let xData = JSON.parse(xReq.responseText);
+       let flowersApi = "https://pixabay.com/api/?key=8455558-58a99ccd0e878ead6f3916c5d&q=rose+flowers&image_type=photo&pretty=true"
+let xReq = new XMLHttpRequest();
+xReq.open('GET', flowersApi);
+xReq.onload = function result() {
+    let xData = JSON.parse(xReq.responseText);
+    let flowers = document.getElementById("flowers");
+    flowers.src = xData.hits[6].previewURL;
+}
+xReq.send();
         let temperatureValue = document.getElementById("temperatureValue");
         temperatureValue.innerHTML = xData.main.temp;  
         let curentWeatherIcon = document.getElementById("curentWeatherIcon");
@@ -93,21 +59,29 @@ function handleForecastWeather() {
         day1.innerHTML = days[d1.getDay()];
         let iconDay1 = document.getElementById("icon_day1");
         iconDay1.src = "./images/icons/" + xData.list[8].weather[0].icon + ".svg";
+        let tempDay1 = document.getElementById("temp_day1");
+        tempDay1.innerHTML = Math.round(xData.list[8].main.temp) + "";
         let d2 = new Date(`${xData.list[16].dt_txt}`);
         let day2 = document.getElementById('day2');
         day2.innerHTML = days[d2.getDay()];
         let iconDay2 = document.getElementById("icon_day2");
         iconDay2.src = "./images/icons/" + xData.list[16].weather[0].icon + ".svg";
+        let tempDay2 = document.getElementById("temp_day2");
+        tempDay2.innerHTML =Math.round(xData.list[16].main.temp);
         let d3 = new Date(`${xData.list[24].dt_txt}`);
         let day3 = document.getElementById('day3');
         day3.innerHTML = days[d3.getDay()];
         let iconDay3 = document.getElementById("icon_day3");
         iconDay3.src = "./images/icons/" + xData.list[24].weather[0].icon + ".svg";
+        let tempDay3 = document.getElementById("temp_day3");
+        tempDay3.innerHTML = Math.round(xData.list[24].main.temp);
         let d4 = new Date(`${xData.list[32].dt_txt}`);
         let day4 = document.getElementById('day4');
         day4.innerHTML = days[d4.getDay()];
         let iconDay4 = document.getElementById("icon_day4");
         iconDay4.src = "./images/icons/" + xData.list[32].weather[0].icon + ".svg";
+        let tempDay4 = document.getElementById("temp_day4");
+        tempDay4.innerHTML = Math.round(xData.list[32].main.temp);
       };
       xReq.send();
 }
@@ -122,3 +96,98 @@ function dayAndDate(){
      currentDate.innerHTML =d.getDate() + " " + months[d.getMonth()];
 }
 dayAndDate();
+
+//Photos start form here
+function awsomePhotos(x){
+    x = 10;
+    let flowersApi = "https://pixabay.com/api/?key=8455558-58a99ccd0e878ead6f3916c5d&q=rose+flowers&image_type=photo&pretty=true"
+    let xReqF = new XMLHttpRequest();
+    xReqF.open('GET', flowersApi);
+    xReqF.onload = function result() {
+        let xData = JSON.parse(xReqF.responseText);
+        let flowers = document.getElementById("flowers");
+        flowers.src = xData.hits[x].previewURL;
+    }
+    xReqF.send();
+
+    let citiesApi = "https://pixabay.com/api/?key=8455558-58a99ccd0e878ead6f3916c5d&q=cities&image_type=photo&pretty=true"
+    let xReqC = new XMLHttpRequest();
+    xReqC.open('GET', citiesApi);
+    xReqC.onload = function result() {
+        let xData = JSON.parse(xReqC.responseText);
+        let flowers = document.getElementById("cities");
+        flowers.src = xData.hits[x].previewURL;
+    }
+    xReqC.send();
+
+    let peopleApi = "https://pixabay.com/api/?key=8455558-58a99ccd0e878ead6f3916c5d&q=people&image_type=photo&pretty=true"
+    let xReqP = new XMLHttpRequest();
+    xReqP.open('GET', peopleApi);
+    xReqP.onload = function result() {
+        let xData = JSON.parse(xReqP.responseText);
+        let people = document.getElementById("people");
+        people.src = xData.hits[x].previewURL;
+    }
+    xReqP.send();
+
+    let winterApi = "https://pixabay.com/api/?key=8455558-58a99ccd0e878ead6f3916c5d&q=winter&image_type=photo&pretty=true"
+    let xReqW = new XMLHttpRequest();
+    xReqW.open('GET', winterApi);
+    xReqW.onload = function result() {
+        let xData = JSON.parse(xReqW.responseText);
+        let winter = document.getElementById("winter");
+        winter.src = xData.hits[x].previewURL;
+    }
+    xReqW.send();
+
+    let springApi = "https://pixabay.com/api/?key=8455558-58a99ccd0e878ead6f3916c5d&q=spring&image_type=photo&pretty=true"
+    let xReqS = new XMLHttpRequest();
+    xReqS.open('GET', springApi);
+    xReqS.onload = function result() {
+        let xData = JSON.parse(xReqS.responseText);
+        let spring = document.getElementById("spring");
+        spring.src = xData.hits[x].previewURL;
+    }
+    xReqS.send();
+
+    let autumnApi = "https://pixabay.com/api/?key=8455558-58a99ccd0e878ead6f3916c5d&q=autumn&image_type=photo&pretty=true"
+    let xReqA = new XMLHttpRequest();
+    xReqA.open('GET', autumnApi);
+    xReqA.onload = function result() {
+        let xData = JSON.parse(xReqA.responseText);
+        let autumn = document.getElementById("autumn");
+        autumn.src = xData.hits[x].previewURL;
+    }
+    xReqA.send();
+
+    let houseApi = "https://pixabay.com/api/?key=8455558-58a99ccd0e878ead6f3916c5d&q=house&image_type=photo&pretty=true"
+    let xReqH = new XMLHttpRequest();
+    xReqH.open('GET', houseApi);
+    xReqH.onload = function result() {
+        let xData = JSON.parse(xReqH.responseText);
+        let house = document.getElementById("house");
+        house.src = xData.hits[x].previewURL;
+    }
+    xReqH.send();
+
+    let wildlifeApi = "https://pixabay.com/api/?key=8455558-58a99ccd0e878ead6f3916c5d&q=wildlife&image_type=photo&pretty=true"
+    let xReqWildlife = new XMLHttpRequest();
+    xReqWildlife.open('GET', wildlifeApi);
+    xReqWildlife.onload = function result() {
+        let xData = JSON.parse(xReqWildlife.responseText);
+        let wildlife = document.getElementById("wildlife");
+        wildlife.src = xData.hits[x].previewURL;
+    }
+    xReqWildlife.send();
+
+    let balloonApi = "https://pixabay.com/api/?key=8455558-58a99ccd0e878ead6f3916c5d&q=balloon&image_type=photo&pretty=true"
+    let xReqBalloon = new XMLHttpRequest();
+    xReqBalloon.open('GET', balloonApi);
+    xReqBalloon.onload = function result() {
+        let xData = JSON.parse(xReqBalloon.responseText);
+        let balloon = document.getElementById("balloon");
+        balloon.src = xData.hits[x].previewURL;
+    }
+    xReqBalloon.send();
+}
+awsomePhotos();
---
+++ js/flowersApi.js
@@ -0,0 +1,9 @@
+// let flowersApi = "https://pixabay.com/api/?key=8455558-58a99ccd0e878ead6f3916c5d&q=rose+flowers&image_type=photo&pretty=true"
+// let xReq = new XMLHttpRequest();
+// xReq.open('GET', flowersApi);
+// xReq.onload = function result() {
+//     let xData = JSON.parse(xReq.responseText);
+//     let flowers = document.getElementById("flowers");
+//     flowers.src = xData.hits[6].previewURL;
+// }
+// xReq.send();
